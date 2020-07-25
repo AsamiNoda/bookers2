8666ddcf7eb8dbbc2e36020294bcfc718b5367a6
github AsamiNoda <asaminoda43@gmail.com>
2020-07-25 18:43:38 +0900

    Initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+	before_action :authenticate_user!
+	def create
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id
+		@user = current_user
+		@books = Book.all
+		if @book.save
+		flash[:notice] = "You have created book successfully."
+		redirect_to book_path(@book.id)
+		else
+		render :index
+
+		end
+	end
+	def index
+		@book = Book.new
+		@books = Book.all
+		@user = current_user
+		@users = User.all
+
+	end
+	def show
+		@book_new = Book.new
+		@book = Book.find(params[:id])
+		@user = @book.user
+	end
+	def update
+		@book_new = Book.new(book_params)
+		@user = current_user
+		@books = Book.all
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+		flash[:notice] = "You have updated book successfully."
+		redirect_to book_path(@book.id)
+		else
+		render :edit
+		end
+
+	end
+	def edit
+		@book = Book.find(params[:id])
+		 if @book.user_id != current_user.id
+    	redirect_to books_path
+    	end
+	end
+
+	def destroy
+		@book = Book.find(params[:id])
+	  	@book.destroy
+	  	redirect_to books_path
+	end
+	private
+	def book_params
+		params.require(:book).permit(:title, :body, :user_id)
+	end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '8484b3f30c3bcbc365915e8d962b03aa169b2a9b688fd5840124b9080c30a177a06b262d11c8a7877169b2d1f2d2b51297667e0319f5f26123cf18b7153b1247'
